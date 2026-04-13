24430dddc2b9c76e5fd4da72f91b3314451b9785
Duman16 <duman29042006@gmail.com>
2026-04-13 22:02:27 +0500

    Сделал страницу для входа без верстки, только логику и возможность захода по JWT токену

--- rent_project/rental_service/rental_service/urls.py
+++ rent_project/rental_service/rental_service/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("core.urls")),
+    #path("api/", include("core.urls")),
 
     # JWT
     path("api/token/", TokenObtainPairView.as_view()),
@@ -15,4 +15,5 @@ urlpatterns = [
     path("api/equipment/", include("equipment.urls")),
     path("api/rentals/", include("rentals.urls")),
     path("api/payments/", include("payments.urls")),
+    path("", include("web.urls")),
 ]
---
+++ rent_project/rental_service/web/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+    error = None
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            error = "Invalid username or password"
+
+    return render(request, "login.html", {"error": error})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html")
